0069a1c016a247ee8e710e33dba70d8bcd16e2ba
madkpi <madkpi@analyticsliv.com>
2024-08-12 16:44:37 +0530

    make major change and add feature screen

--- src/app/(private)/feature/page.tsx
+++ src/app/(private)/feature/page.tsx
@@ -1,16 +1,17 @@
-// import PhoneSideBar from "@/components/OrderHistory/PhoneSideBar";
-// import Breadcrumbs from "@/components/shared/Breadcrumb/Breadcrumb";
+"use client";
+import { useCheckAuth } from "@/components/hooks/useCheckAuth";
 import React from "react";
 
 const page = ({ }: any) => {
+    const  { loading, session } = useCheckAuth()
   return (
     <>
-      <h1>in Acount</h1>
+      <h1>iIn feature screen if user is logedin</h1>
 
       <div className="md:hidden">
         <div className="flex flex-row items-center justify-center my-8 text-lg font-medium">
           {/* <Breadcrumbs paths={[{ label: "Home" }, { label: "My Account" }]} /> */}
-          <h1>in Acount</h1>
+          <h1>In feature screen if user is logedin </h1>
         </div>
         {/* <PhoneSideBar activeTab={"Profile"} /> */}
       </div>
--- src/app/(private)/wishlist/loading.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-const loading = () => {
-  return (
-    <div>loading</div>
-  )
-}
-
-export default loading
