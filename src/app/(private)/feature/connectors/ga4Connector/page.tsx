ff73ce7fec56c044c064c93ea19be8a6af2323d3
pruthviraj0306 <pruthvirajsinh.rathod@analyticsliv.com>
2024-09-02 17:23:23 +0530

    Buttons disabled and styling of ga4 and fb connector pages

--- src/app/(private)/feature/connectors/ga4Connector/page.tsx
+++ src/app/(private)/feature/connectors/ga4Connector/page.tsx
@@ -21,10 +21,21 @@ const GA4ConnectorPage = () => {
   }, []);
 
   return (
-    <div>
-      <h1 className='pb-5 text-base'>Authorize Google Analytics Access</h1>
-      <a href={authUrl} className='border border-indigo-900 p-2 cursor-pointer hover:bg-blue-950 hover:text-white font-medium'> &gt; Authorize</a>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-[#F6F8FE] to-[#E8EAF6]">
+    <div className="bg-white shadow-lg rounded-lg p-8 text-center mx-auto">
+      <h1 className="pb-5 text-3xl font-semibold text-gray-800">Authorize Google Analytics Access</h1>
+      <a 
+        href={authUrl} 
+        className="inline-block bg-primary text-xl text-white py-3 px-6 rounded-full shadow-md transition-all duration-100 ease-in-out transform hover:bg-gray-700 hover:scale-105"
+      >
+        Authorize
+      </a>
     </div>
+  </div>
+    // <div>
+    //   <h1 className='pb-5 text-base'>Authorize Google Analytics Access</h1>
+    //   <a href={authUrl} className='border border-indigo-900 p-2 cursor-pointer hover:bg-blue-950 hover:text-white font-medium'> &gt; Authorize</a>
+    // </div>
   );
 };
 
