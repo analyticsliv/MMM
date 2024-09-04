a4f5ea44b74ef212ff8a5ee977b423a67f67f240
madkpi <madkpi@analyticsliv.com>
2024-09-04 16:05:43 +0530

    facebook connectioor store in db

--- src/app/(private)/feature/connectors/facebookConnector/Sucess/page.tsx
+++ src/app/(private)/feature/connectors/facebookConnector/Sucess/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
+import useConnector from '@/components/hooks/connectors/useConnectors';
+import { useSession } from 'next-auth/react';
 
 const SuccessPage = () => {
+  const { data: session, status } = useSession();
+  const { updateOrCreateConnector, getConnectorData, error, loading } = useConnector()
   const searchParams = useSearchParams();
   const accessToken = searchParams.get('accessToken');
   const [accounts, setAccounts] = useState<any[]>([]);
@@ -10,12 +14,22 @@ const SuccessPage = () => {
 
   useEffect(() => {
     if (accessToken) {
-      console.log('Access Token:', accessToken);
       fetchUserAccounts(accessToken);
     }
   }, [accessToken]);
 
-  const fetchUserAccounts = async (accessToken:string) => {
+  useEffect(() => {
+    if (accessToken) {
+      const connectorData = {
+        accessToken: accessToken,
+        expire: Date.now() + 60 * 24 * 60 * 60 * 1000
+      };
+
+      updateOrCreateConnector("data.analytics@analyticsliv.com", 'facebook', connectorData);
+    }
+  }, [accessToken, session, status])
+
+  const fetchUserAccounts = async (accessToken: string) => {
     try {
       const response = await fetch(`https://graph.facebook.com/v11.0/me/accounts?access_token=${accessToken}`);
       const data = await response.json();
@@ -28,7 +42,7 @@ const SuccessPage = () => {
   const handleAccountSelect = async (event: React.ChangeEvent<HTMLSelectElement>) => {
     const accountId = event.target.value;
     setSelectedAccount(accountId);
-    console.log("ghjkl",accountId)
+    console.log("ghjkl", accountId)
   };
 
   return (
