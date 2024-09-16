c6cc5a342628ea26191df3ff36e60bf5bed29a05
madkpi <madkpi@analyticsliv.com>
2024-09-16 14:32:08 +0530

    make minor changes in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    experimental: {
+        appDir: true,
+    },
     webpack: (config, { isServer }) => {
         if (!isServer) {
             config.resolve.fallback = {
