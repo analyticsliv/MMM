76a627ccf621c5d161d681a9b292ff02790b0f37
pruthviraj0306 <pruthvirajsinh.rathod@analyticsliv.com>
2024-11-26 17:11:54 +0530

    Coming soon for not available page in navbar

---
+++ src/app/(private)/feature/admin/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+
+const Admin = () => {
+
+    return (
+        <>
+            <div className="flex h-screen items-center justify-center bg-gray-100">
+                <div className="text-center animate-fadeIn">
+                    <h1 className="text-5xl font-bold text-gray-800 mb-4">🚀 Coming Soon!</h1>
+                    <p className="text-lg text-gray-600">This page is currently under development. Stay tuned for exciting updates!</p>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Admin;
