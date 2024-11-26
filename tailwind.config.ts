5da6c1dcdffa6f71a17f91c074495c1d89d817aa
pruthviraj0306 <pruthvirajsinh.rathod@analyticsliv.com>
2024-11-26 17:15:58 +0530

    animation for not available changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
   theme: {
     container: {
@@ -21,28 +21,28 @@ const config = {
       backgroundImage: {
         'custom-gradient': 'linear-gradient(to right, #01A2FC 100%, #36D2EF 100%)',
       },
-      colors:{
-        background:"#F0F0F9",
-        foreground:"#020044",
-        homeGray:"#EDF4FF",
-        primary:"#30486A",
-        homebg:"#FFF4EE",
-        textcolor:"#202020",
+      colors: {
+        background: "#F0F0F9",
+        foreground: "#020044",
+        homeGray: "#EDF4FF",
+        primary: "#30486A",
+        homebg: "#FFF4EE",
+        textcolor: "#202020",
         black: '#000',
         white: '#FFFFFF',
         gray: {
-          default:'#030822',
-          100:"#F9F9FB",
-          200:"#FAF9F8",
-          300:"#F4F5F6",
-          400:"#E9E9E9",
-          500:"#C3C9D6",
-          600:"#8991A4",
-          700:"#030822"
+          default: '#030822',
+          100: "#F9F9FB",
+          200: "#FAF9F8",
+          300: "#F4F5F6",
+          400: "#E9E9E9",
+          500: "#C3C9D6",
+          600: "#8991A4",
+          700: "#030822"
         },
       },
       fontFamily: {
-        jost : ['Jost', 'sans-serif'],
+        jost: ['Jost', 'sans-serif'],
       },
       borderRadius: {
         lg: "20px",
@@ -50,6 +50,10 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+        "fadeIn": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -60,6 +64,7 @@ const config = {
         },
       },
       animation: {
+        "fadeIn": "fadeIn 1.5s ease-in-out",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
