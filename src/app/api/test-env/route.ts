e1caab64465029600fa07c32a2ae8a5c1c42c142
vermaAtul1520 <atulverma1520@gmail.com>
2025-07-16 13:31:55 +0530

    DV360_REDIRECT_URI added in test route

--- src/app/api/test-env/route.ts
+++ src/app/api/test-env/route.ts
@@ -3,5 +3,6 @@ export async function GET() {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID ? 'Set' : 'Not set',
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET ? 'Set' : 'Not set',
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET ? 'Set' : 'Not set',
+    DV360_REDIRECT_URI : process.env.DV360_REDIRECT_URI ? 'Set' : 'Not set'
   });
 }
