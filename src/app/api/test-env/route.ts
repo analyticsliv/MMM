7795e428f64ef9fc9aac423c827a872926633d28
vermaAtul1520 <atulverma1520@gmail.com>
2025-07-16 14:30:13 +0530

    LINKEDIN_CLIENT_ID added in test route

--- src/app/api/test-env/route.ts
+++ src/app/api/test-env/route.ts
@@ -3,7 +3,7 @@ export async function GET() {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID ? 'Set' : 'Not set',
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET ? 'Set' : 'Not set',
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET ? 'Set' : 'Not set',
-    DV360_REDIRECT_URI : process.env.DV360_REDIRECT_URI ? 'Set' : 'Not set',
+    DV360_REDIRECT_URI : process.env.DV360_REDIRECT_URI ,
     LINKEDIN_CLIENT_ID : process.env.LINKEDIN_CLIENT_ID  ? 'Set' : 'Note set'
   });
 }
