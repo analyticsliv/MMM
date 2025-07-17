6745002e02bbb60a3ddfb308e3a9f215f66457f0
vermaAtul1520 <atulverma1520@gmail.com>
2025-07-17 15:49:16 +0530

    minor change in test api

--- src/app/api/test-env/route.ts
+++ src/app/api/test-env/route.ts
@@ -1,9 +1,9 @@
 export async function GET() {
   return Response.json({
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID ? 'Set' : 'Not set',
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET ? 'Set' : 'Not set',
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET ? 'Set' : 'Not set',
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID ,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET ,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET ,
     DV360_REDIRECT_URI : process.env.DV360_REDIRECT_URI ,
-    LINKEDIN_CLIENT_ID : process.env.LINKEDIN_CLIENT_ID  ? 'Set' : 'Note set'
+    LINKEDIN_CLIENT_ID : process.env.LINKEDIN_CLIENT_ID  
   });
 }
